2cf3e2e6f5976d7e65839012059d3cce760c8ad6
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2024-04-27 00:01:05 +0200

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 import { store, persistor } from 'store/store';
@@ -13,9 +13,9 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <HashRouter basename="/rental-of-campers">
+      <BrowserRouter basename="/rental-of-campers">
         <App />
-      </HashRouter>
+      </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
