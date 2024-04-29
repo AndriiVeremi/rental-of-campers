a30e20c01897ab43155cec8fb64dce3099e30219
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2024-04-29 13:01:29 +0200

    add

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -4,18 +4,15 @@ export const selectCampers = state => state.adverts.advert.campers;
 export const selectRespLength = state => state.adverts.advert.length;
 
 export const selectAllCampers = state => state.adverts.filters.items;
-export const selectFiltered = state => state.adverts.filters.filters
+export const selectFiltered = state => state.adverts.filters.filters;
 
 export const selectIsLoading = state => state.adverts.isLoading;
 export const selectError = state => state.adverts.error;
 
 export const selectFavorites = state => state.favorites.items;
 
-;
-
 export const selectFilteredCampers = createSelector([selectAllCampers, selectFiltered], (campers, filters) => {
   return campers.filter(item => {
-
     if (!item.location.toLowerCase().includes(filters.location.toLowerCase())) {
       return false;
     }
@@ -34,10 +31,7 @@ export const selectFilteredCampers = createSelector([selectAllCampers, selectFil
     }
 
     const form = filters.form.vehicleType;
-    console.log('message', form.toLowerCase())
-    console.log('message2', item.form.toLowerCase())
-
-    if (item.form.toLowerCase() !== form.toLowerCase() &&  form !== '' ) {
+    if (item.form.toLowerCase() !== form.toLowerCase() && form !== '') {
       return false;
     }
 
