2bc30f23bec01130f1a4660853c8fc5318770997
Andrii Veremii <114438382+AndriiVeremi@users.noreply.github.com>
2024-04-29 12:22:16 +0200

    add fix Home page

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,8 +1,19 @@
-// import HomeImg from '../../images/pictures/home.jpg';
+import HomeImg from '../../images/pictures/home.png';
 import Hero from 'components/Home/Hero/Hero';
 import HeroContent from 'components/Home/HeroContent/HeroContent';
 
-import { InfoWrapper, InfoTitle, DescriptionTitle, HeroWrapper, ContentDescription, TextTitle } from './HomePage.module';
+import {
+  InfoWrapper,
+  InfoTitle,
+  DescriptionTitle,
+  PlanningList,
+  PlanningTitle,
+  PlanningItem,
+  HeroWrapper,
+  ContentDescription,
+  SubTitlePlanning,
+  InfoImg,
+} from './HomePage.module';
 
 const HomePage = () => {
   return (
@@ -12,49 +23,45 @@ const HomePage = () => {
       </HeroWrapper>
 
       <InfoWrapper>
-        <InfoTitle>Welcome to Rental Of Campers!</InfoTitle>
+        <InfoTitle>Welcome to Rental Of Campers </InfoTitle>
         <DescriptionTitle>
           Embark on your next adventure with our fleet of top-quality campers. Whether you're planning a weekend getaway
           or a cross-country road trip, we have the perfect camper to suit your needs.
         </DescriptionTitle>
 
         <HeroContent />
-
-        {/* <InfoImg src={HomeImg} alt="HomeImg" /> */}
       </InfoWrapper>
 
       <ContentDescription>
-          <TextTitle>
-            Embark on your next adventure with our fleet of top-quality campers. Whether you're planning a weekend
-            getaway or a cross-country road trip, we have the perfect camper to suit your needs.
-          </TextTitle>
-          <h2>Why choose us?</h2>
-          <ul>
-            <li>
+        <div>
+          <PlanningTitle>Why choose us?</PlanningTitle>
+          <PlanningList>
+            <PlanningItem>
               <strong>Diverse Fleet:</strong> From cozy campervans to spacious RVs, we offer a wide range of options to
               accommodate solo travelers, couples, families, and groups of all sizes.
-            </li>
-            <li>
+            </PlanningItem>
+            <PlanningItem>
               <strong>Quality Assurance:</strong> Our campers undergo regular maintenance checks to ensure they are in
               excellent condition, providing you with a safe and comfortable travel experience.
-            </li>
-            <li>
+            </PlanningItem>
+            <PlanningItem>
               <strong>Easy Booking:</strong> With our user-friendly website and efficient booking system, securing your
-              camper rental is quick and hassle-free. Simply choose your dates, select your preferred camper, and you're
-              ready to hit the road!
-            </li>
-            <li>
+              camper rental is quick and hassle-free.
+            </PlanningItem>
+            <PlanningItem>
               <strong>Expert Support:</strong> Have questions or need assistance? Our friendly and knowledgeable team is
               here to help. Whether you need travel tips or technical support during your journey, we've got you
               covered.
-            </li>
-          </ul>
-          <div>
-            Start planning your next adventure today! Browse our selection of campers and{' '}
-            <div>book your rental online</div>
-            Your unforgettable journey awaits with Camper Rentals.
-          </div>
-        </ContentDescription>
+            </PlanningItem>
+          </PlanningList>
+        </div>
+        <SubTitlePlanning>
+          <InfoImg src={HomeImg} alt="HomeImg" />
+          Start planning your next adventure today! Browse our selection of campers and{' '}
+          <div>book your rental online</div>
+          Your unforgettable journey awaits with Camper Rentals.
+        </SubTitlePlanning>
+      </ContentDescription>
     </>
   );
 };
--- src/pages/HomePage/HomePage.module.js
+++ src/pages/HomePage/HomePage.module.js
@@ -13,15 +13,17 @@ export const InfoWrapper = styled.div`
 
 export const InfoImg = styled.img`
   width: 350px;
-  margin-bottom: 60px;
-  width: 400px;
-  height: 254px;
+  margin-bottom: 10px;
+  width: 300px;
+  height: 154px;
 `;
 
 export const InfoTitle = styled.h1`
   font-size: 50px;
   margin-bottom: 100px;
-  color: #e44848;
+  color: #474246;
+  /* color: #e44848; */
+  display: flex;
 `;
 
 export const DescriptionTitle = styled.p`
@@ -31,20 +33,40 @@ export const DescriptionTitle = styled.p`
   text-align: center;
   color: #474246;
 `;
+
 export const ContentDescription = styled.div`
-background-color:  #e44848;
+background-color: #fce4e4;
+border: 1px solid #b02525;
 padding: 60px;
 border-radius: 20px;
-color: #fff;
+color: #474246;
 margin-bottom: 60px;
+display: flex;
+gap: 100px;
 `;
 
-export const TextTitle = styled.p`
-/* display: flex;
-justify-content: center; */
-width: 800px;
-text-align: center;
+export const PlanningTitle = styled.h2`
+margin-bottom: 20px;
+font-size: 28px;
+`;
 
+export const PlanningList = styled.ul`
+display: flex;
+flex-direction: column;
+gap: 20px;
+`;
+
+export const PlanningItem = styled.li`
+
+`;
+
+export const SubTitlePlanning = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
+text-align: center;
 font-style: italic;
 font-size: 20px;
 `;
+
